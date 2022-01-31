079053241c3519a0404f08e0e7dcadebc3dbee15
Thanh Nguyen <chrisng9@outlook.com>
2022-01-30 17:20:28 -0800

    Added comments to OCP example code

--- ocp/main.go
+++ ocp/main.go
@@ -2,17 +2,18 @@ package main
 
 import "fmt"
 
-// OCP
+// OCP - Open for intentiate but close for midification pattern
 // open for extension, close for modification
-//
 
+// Color defines Color as an int
 type Color int
 
+// Color = iota assign 0 to Color and increment by 1 down the color list
 const (
-	brown Color = iota
-	red
-	orange
-	yellow
+	brown  Color = iota // so brown is type Color and has value of 0
+	red                 // red has value or 0 + 1 = 1
+	orange              // orange = 2
+	yellow              // = 3
 	green
 	blue
 	purple
@@ -21,29 +22,34 @@ const (
 	black
 )
 
+// Size defines Size as an int
 type Size int
 
+// Size = iota assign 0 to first size in the list and
+// increment by 1 down list
 const (
-	small Size = iota
-	medium
+	small  Size = iota // small is type Size with value of 0
+	medium             // = 1
 	large
 	xlarge
 	xxlarge
 )
 
+// Product structure hold basic Product name, color, and size
 type Product struct {
 	name  string
 	color Color
 	size  Size
 }
 
+// Filter defines and empty Filter struct
 type Filter struct {
 	//
 }
 
+// FilterByClolor compares color with color in product slice
 func (f *Filter) FilterByClolor(p []Product, c Color) []*Product {
 	result := make([]*Product, 0)
-
 	for i, v := range p {
 		if v.color == c {
 			result = append(result, &p[i])
@@ -52,29 +58,37 @@ func (f *Filter) FilterByClolor(p []Product, c Color) []*Product {
 	return result
 }
 
-// Specification pattern
+/////////// Specification pattern //////////////////
 type Specification interface {
 	IsSatisfied(p *Product) bool
 }
 
+// ColorSpec struct
 type ColorSpec struct {
 	color Color
 }
 
+// IsSatisfied compares ColorSpec field color with Product's
+// color field and return a boolean value
 func (c ColorSpec) IsSatisfied(p *Product) bool {
 	return p.color == c.color
 }
 
+// SizeSpec tructure
 type SizeSpec struct {
 	size Size
 }
 
+// IsSatisfied method compare SizeSpec's size with Product's
+// size field and return a boolean value
 func (s SizeSpec) IsSatisfied(p *Product) bool {
 	return p.size == s.size
 }
 
+// BatterFilter is and empty struct
 type BetterFilter struct{}
 
+// Filter method compares Specification's field to Product's fields
 func (f *BetterFilter) Filter(p []Product, s Specification) []*Product {
 	result := make([]*Product, 0)
 	for i, v := range p {
@@ -85,11 +99,16 @@ func (f *BetterFilter) Filter(p []Product, s Specification) []*Product {
 	return result
 }
 
-// Composite Specification pattern
+/////////////////// Composite Specification pattern /////////////////////
+
+// CombineSpec defines first and second fields that the Filter must
+// match in order to satisfy the search
 type CombineSpec struct {
 	first, second Specification
 }
 
+// IsSatisfied compares first and second fields of the Product's
+//
 func (c CombineSpec) IsSatisfied(p *Product) bool {
 	return c.first.IsSatisfied(p) && c.second.IsSatisfied(p)
 }
@@ -108,18 +127,20 @@ func main() {
 	}
 	//
 	fmt.Println("Green products (new):")
-	greenSpec := ColorSpec{green}
+	greenSpec := ColorSpec{green} // greenSpec variable
 	bf := BetterFilter{}
 	for _, v := range bf.Filter(products, greenSpec) {
 		fmt.Printf(" - %s is green\n", v.name)
 	}
-
+	// Set the SizeSpec to large and CombineSpec's first = greenSpec, second = large
 	lSpec := SizeSpec{large}
-	lgSpec := CombineSpec{greenSpec, lSpec}
-
+	lgSpec := CombineSpec{
+		first:  greenSpec, // define online 131 or command // greenSpec variable
+		second: lSpec,
+	}
+	// Print filter results from Filter method
 	fmt.Println("Combine Large & Green products:")
 	for _, v := range bf.Filter(products, lgSpec) {
 		fmt.Printf(" - %s green\n", v.name)
 	}
-
 }
