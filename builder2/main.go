1111600c636b4bc881b6e762e0569c74700cac3d
Thanh Nguyen <chrisng9@outlook.com>
2022-02-14 21:04:37 -0800

    Added AddChildFluent method to builder

--- builder2/main.go
+++ builder2/main.go
@@ -57,10 +57,25 @@ func (b *HTMLBuilder) AddChild(chName, chText string) {
 	b.root.elements = append(b.root.elements, e)
 }
 
+// AddChildFluent
+func (b *HTMLBuilder) AddChildFluent(chName, chText string) *HTMLBuilder {
+	e := HTMLElement{tag: chName, text: chText, elements: []HTMLElement{}}
+	b.root.elements = append(b.root.elements, e)
+	return b
+}
+
 // Program entry
 func main() {
 	nb := NewHTMLBuilder("html")
 	nb.AddChild("head", "")
 	nb.AddChild("body", "<h1>Hello Builder Design Patter</h1><p>Design patter is amazin and fun to learn</p>")
 	fmt.Println(nb.String())
+
+	ul := NewHTMLBuilder("html")
+	ul.AddChild("head", "")
+	ul.AddChildFluent("body", "<h1>Welcome to Build Patter!</h1>").
+		AddChildFluent("ul", "").AddChildFluent("li", "Item 1").
+		AddChildFluent("li", "Item 2").AddChildFluent("li", "Item 3").
+		AddChildFluent("li", "Item 4")
+	fmt.Println(ul.String())
 }
